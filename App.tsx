dc8dfee4383d7bd40c7ed04b36a4ed3ec44cdcdf
Kim-777 <60419368+Kim-777@users.noreply.github.com>
2021-06-24 13:19:29 +0900

    setting: assets

--- App.tsx
+++ App.tsx
@@ -1,47 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, SafeAreaView, TouchableOpacity, TouchableHighlight, Text, TextInput, Alert, Button } from 'react-native';
+// prettier-ignore
+import { StyleSheet, SafeAreaView, TouchableOpacity, TouchableHighlight, Text, View, TextInput, Alert, Button, Platform, Dimensions, ImageBackground, Image } from 'react-native';
 import * as D from './src/data';
 import Person from './src/screens/Person';
+import { Colors } from 'react-native-paper';
+import Color from 'color';
+
+const avatarUrl = D.randomAvatarUrl();
+const avatarSize = 50
+
+console.log(Platform.OS)
+const { width, height } = Dimensions.get('window');
 
 
-const people = D.makeArray(100).map(D.createRandomPerson)
-
-const person = D.createRandomPerson();
 
 export default function App() {
 
-  const children = people.map((person) => (
-    <Person key={person.id} person={person} />
-  ))
-
-  const onPress = () => Alert.alert('pressed.', 'message')
-
   return (
-    <SafeAreaView style={styles.container}>
-      <Button title="Home" onPress={onPress} />
-      <TouchableOpacity onPress={onPress}>
-        <Text>Opacity</Text>
-      </TouchableOpacity>
-      <TouchableHighlight onPress={onPress}>
-        <Text>Highlight</Text>
-      </TouchableHighlight>
-      <TextInput
-        placeholder="enter your name"
-        onChangeText={(text: string): void => console.log(text)}
-        onFocus={() => console.log('onFocus')}
-        onBlur={() => console.log('onBlur')}
-        onEndEditing={() => console.log('onEndEditing')}
-      />
+    <SafeAreaView style={[styles.flex]}>
+      <ImageBackground
+        style={[styles.flex, styles.imageBackground]}
+        source={require('./src/assets/images/bg.jpg')}
+      >
+        <Image source={{uri: avatarUrl}} style={[styles.image]}/>
+      </ImageBackground> 
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  flex: {
+    flex: 1
+  },
+  imageBackground: {
+    padding: 10
+  },
+  image: {
+    width: avatarSize, height: avatarSize, borderRadius: avatarSize / 2
   },
 });
